c9e3b66e341f3710e23c1c8668507ab53cbf8c61
alexthegithubber <alexander.wintzeus@gmail.com>
2024-08-23 16:56:19 +0200

    Added outputs to JS action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -14,7 +14,9 @@ function run() {
     const s3Uri = `s3://${bucket}`;
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
-    core.notice('Hello from my custom JavaScript action!');
+    // 3) Generate website url
+    const websiteUrl = `http://${bucket}.s3-website.${bucketRegion}.amazonaws.com`;
+    core.setOutput('website-url', websiteUrl);
 }
 
 run();
